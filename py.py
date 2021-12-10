230e348fe37e4ef9d2197cf25a113e5fd5a0416e
Vojtech <vojtab42@gmail.com>
2021-12-10 18:27:46 +0100

    removed empty lines

--- py.py
+++ py.py
@@ -1,14 +1,9 @@
 name="Vojtech"
-
 print("Hello this is my (" + name + ") repository, \nfor learnig python and git basics.")
-
 print("\n Thank you for your attention. :) ")
-
 print("now on main here we go")
 
-
 t=True
-
 if t :{
     print("The T variable is true")
 }

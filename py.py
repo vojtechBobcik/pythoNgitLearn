ee158d3f39f0bef13079b0d790b8ab706e5f2076
Vojtech <vojtab42@gmail.com>
2021-12-10 18:17:12 +0100

    if statement added

--- py.py
+++ py.py
@@ -3,3 +3,12 @@ name="Vojtech"
 print("Hello this is my (" + name + ") repository, \nfor learnig python and git basics.")
 
 print("\n Thank you for your attention. :) ")
+
+print("now on main here we go")
+
+
+t=True
+
+if t :{
+    print("The T variable is true")
+}
